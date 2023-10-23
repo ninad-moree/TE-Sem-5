0e05f786fe339f35731b7479845ebbb930eb5521
Ninad More <ninadyogeshmore@gmail.com>
2023-10-23 18:18:28 +0530

    db-asgn9 updated

--- DBMS/Asgn 9.js
+++ DBMS/Asgn 9.js
@@ -1,41 +1,43 @@
-// 1) Insert Operations
-db.product.insertOne({pid: 4, pname: "ABC", size: "S"});
+// 1) INSERT OPERATIONS:
+db.Product.insertOne({pid: 4, pname: "ABC", size:"S", price: 29, rating: 4})
 
-db.product.insertMany([
-{pid: 2, pname: "DEF", size: "M"}, 
-{pid: 3, pname: "GHK", size: "L", brand: {company: "Nike", material: "Cotton"}}
+db.Product.insertOne({pid: 1, pname: "DEF", size:"S", price: 110, rating: 4.5})
+
+db.Product.insertMany([
+    {pid: 2, pname: "GHK", size: "M", price: 199, rating: 3.7},
+    {pid: 3, pname: "LKM", size: "L", price: 69, rating: 3.4},
+    {pid: 5, pname: "NHK", size: "L", price: 299, rating: 4.9},
+    {pid: 6, pname: "TUV", size: "S", price: 100, rating: 3.0}
 ]);
 
-// 2) Read Operation
-db.product.find().pretty();
-// {
-//     "_id" : ObjectId("652e008be0708de849b8c7db"),
-//     "pid" : 4,
-//     "pname" : "ABC",
-//     "size" : "S"
-// }
-// {
-//     "_id" : ObjectId("652e01afe0708de849b8c7dc"),
-//     "pid" : 2,
-//     "pname" : "DEF",
-//     "size" : "M"
-// }
-// {
-//     "_id" : ObjectId("652e01afe0708de849b8c7dd"),
-//     "pid" : 3,
-//     "pname" : "GHK",
-//     "size" : "L",
-//     "brand" : {
-//    	 "company" : "Nike",
-//    	 "material" : "Cotton"
-//     }
-// }
 
-// 3) Delete Operation
-db.product.deleteOne({ pname: "DEF" })
+// 2) READ OPERATIONS
+db.Product.find()
 
-// 4) Update Operation
-db.product.updateOne( {pid: 4}, { $set: {pname: "XYZ"} } )
+db.Product.find({rating: {$gt: 3}})
 
-// 5) Save Operation
-db.product.save( {pid: 1, pname: "ABC", size: "S"} )
+
+// 3) DELETE OPERATIONS
+db.Product.deleteOne({pname: "GHK"})
+
+// 4) UPDATE OPERATIONS
+db.Product.updateOne({pname: "NHK"}, {$set: {price: 169}})
+
+// 5) OPERATORS
+
+// a-AND
+db.Product.find({
+    price: { $lt: 150},
+    rating: {$gt: 3.5}
+})
+
+// b-OR
+db.Product.find({
+    $or: [
+      {price: {$gt: 100}},
+      {size: "S"}
+    ]
+})
+
+
+
