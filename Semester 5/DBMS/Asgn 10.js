50deb2a1baf80a87a7a196bf0de977adc3a680c9
Ninad More <ninadyogeshmore@gmail.com>
2023-11-05 11:11:34 +0530

    asgn 9 and 10 updated

--- Semester 5/DBMS/Asgn 10.js
+++ Semester 5/DBMS/Asgn 10.js
@@ -1,162 +1,104 @@
-db.createCollection('website');
+db.Student.insertOne({
+    name: "Ninad",
+    branch: 'CE',
+    rollno: 31253,
+    scores: [
+        { sub: "DBMS", marks: 90},
+        { sub: "Lp1", marks: 97 },
+        { sub: "CNS", marks: 86}
+    ]
+});
 
-db.website.insert({'roll':'1','name':'harsh','amount':1000,'url':'www.yahoo.com'})
-db.website.insert({'roll':'2','name':'jitesh','amount':2000,'url':'www.yahoo.com'});
-db.website.insert({'roll':'3','name':'rina','amount':3000,'url':'www.google.com'});
-db.website.insert({'roll':'4','name':'ash','amount':4000,'url':'www.gmail.com'});
-db.website.insert({'roll':'5','name':'ash','amount':1000,'url':'www.pvg.com'});
+// AGGREGATION
+db.Student.aggregate({
+    $match: { branch: "CE" }
+});
 
-// ===============================
-// 1) Aggregation
-// ===============================
-
-// SUM AGGREGATE
-db.website.aggregate({$group: {_id:"$name", "total": {$sum:"$amount"}}});
-// { "_id" : "ash", "total" : 5000 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 2000 }
-
-// AVG AGGREGATE 
-db.website.aggregate({$group: {_id:"$name", "total": {$avg:"$amount"}}});
-// { "_id" : "ash", "total" : 2500 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 1000 }
-
-// MIN AGGREGATION 
-db.website.aggregate({$group:{_id:"$name","total":{$min:"$amount"}}});
-// { "_id" : "ash", "total" : 1000 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 1000 }
-
-// MAX AGGREGATION 
-db.website.aggregate({$group:{_id:"$name","total":{$max:"$amount"}}});
-// { "_id" : "ash", "total" : 4000 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 1000 }
-
-// FIRST AGGREGATION 
-db.website.aggregate({$group:{_id:"$name","total":{$first:"$amount"}}});
-// { "_id" : "ash", "total" : 4000 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 1000 }
-
-// LAST AGGREGATION 
-db.website.aggregate({$group:{_id:"$name","total":{$last:"$amount"}}});
-// { "_id" : "ash", "total" : 1000 }
-// { "_id" : "rina", "total" : 3000 }
-// { "_id" : "jitesh", "total" : 2000 }
-// { "_id" : "harsh", "total" : 1000 }
-
-// PUSH AGGREGATION 
-db.website.aggregate({$group:{_id:"$name","total":{$push:"$amount"}}});
-// { "_id" : "ash", "total" : [ 4000, 1000 ] }
-// { "_id" : "rina", "total" : [ 3000 ] }
-// { "_id" : "jitesh", "total" : [ 2000 ] }
-// { "_id" : "harsh", "total" : [ 1000, 1000 ] }
-
-// COUNT AGGREGATION
-db.website.aggregate({$group:{_id:"$name","total": {$sum:1}}});
-// { "_id" : "ash", "total" : 2 }
-// { "_id" : "rina", "total" : 1 }
-// { "_id" : "jitesh", "total" : 1 }
-// { "_id" : "harsh", "total" : 2 }
-
-// ADDTOSET AGGREGATE 
-db.website.aggregate({$group:{_id:"$name","total": {$addToSet:"$amount"}}});
-// { "_id" : "ash", "total" : [ 1000, 4000 ] }
-// { "_id" : "rina", "total" : [ 3000 ] }
-// { "_id" : "jitesh", "total" : [ 2000 ] }
-// { "_id" : "harsh", "total" : [ 1000]}
-
-// ===============================
-// 2) Indexing
-// ===============================
-
-db.createCollection('website1')
-
-db.website1.insert({'r':1,'name':'harsh'}); 
-
-db.website1.createIndex({'name':1})
-
-// CREATE INDEXING 
-db.website1.createIndex({'name':-1})
-// {
-//     "createdCollectionAutomatically" : false,
-//     "numIndexesBefore" : 2,
-//     "numIndexesAfter" : 3,
-//     "ok" : 1
-// }
-
-
-db.website1.getIndexes()
-[
+db.Student.aggregate([
     {
-        "v" : 1,
-        "key" : {"_id" : 1},
-        "name" : "_id_",
-        "ns" : "harsh.website1"
+        $unwind: "$scores" 
     },
     {
-        "v" : 1,
-        "key" : {"name" : 1},
-        "name" : "name_1",
-        "ns" : "harsh.website1"
-    },
-    {
-        "v" : 1,
-        "key" : {"name" : -1},
-        "name" : "name_-1",
-        "ns" : "harsh.website1"
+        $group: {
+            _id: "$rollno",
+            totalMarks: { $sum: "$scores.marks" } //$avg, $min, $max, $first, $last, $push
+        }
     }
-]
+]);
 
-// DROP INDEX
-db.website.dropIndex({'name':-1})
-
-db.website1.dropIndex({'name':1})
-
-
-// GET INDEXING
-db.website1.getIndexes()
-[
-    {
-        "v" : 1,
-        "key" : {"_id" : 1},
-        "name" : "_id_",
-        "ns" : "harsh.website1"
-    }
-]
-
-db.website1.find().pretty()
-
-db.website1.createIndex({'name':1})
-// {
-//     "createdCollectionAutomatically" : false,
-//     "numIndexesBefore" : 1,
-//     "numIndexesAfter" : 2,
-//     "ok" : 1
-// }
+db.Student.aggregate([
+    {$match: {rollno: 31253}},
+    {$unwind: '$sores'},
+]);
 
 
-db.website1.getIndexes()
-[
-    {
-        "v" : 1,
-        "key" : {"_id" : 1},
-        "name" : "_id_",
-        "ns" : "harsh.website1"
-    },
-    {
-        "v" : 1,
-        "key" : {"name" : 1},
-        "name" : "name_1",
-        "ns" : "harsh.website1"
-    }
-]
+//INDEXING
+db.Student.createIndex({name: 1});
+db.Student.getIndexs();
+db.Student.dropIndex({name: 1});
 
-db.website1.dropIndex({'name':1})
+db.Student.createIndex({name: 1, rollno:1});
+
+db.Student.distinct("name");
+
+// // ===============================
+// // 1) Aggregation
+// // ===============================
+
+// // SUM AGGREGATE
+// db.website.aggregate({$group: {_id:"$name", "total": {$sum:"$amount"}}});
+
+// // AVG AGGREGATE 
+// db.website.aggregate({$group: {_id:"$name", "total": {$avg:"$amount"}}});
+
+// // MIN AGGREGATION 
+// db.website.aggregate({$group:{_id:"$name","total":{$min:"$amount"}}});
+
+// // MAX AGGREGATION 
+// db.website.aggregate({$group:{_id:"$name","total":{$max:"$amount"}}});
+
+// // FIRST AGGREGATION 
+// db.website.aggregate({$group:{_id:"$name","total":{$first:"$amount"}}});
+
+// // LAST AGGREGATION 
+// db.website.aggregate({$group:{_id:"$name","total":{$last:"$amount"}}});
+
+// // PUSH AGGREGATION 
+// db.website.aggregate({$group:{_id:"$name","total":{$push:"$amount"}}});
+
+// // COUNT AGGREGATION
+// db.website.aggregate({$group:{_id:"$name","total": {$sum:1}}});
+
+// // ADDTOSET AGGREGATE 
+// db.website.aggregate({$group:{_id:"$name","total": {$addToSet:"$amount"}}});
+
+// // ===============================
+// // 2) Indexing
+// // ===============================
+
+// db.createCollection('website1')
+
+// db.website1.insert({'r':1,'name':'harsh'}); 
+
+// db.website1.createIndex({'name':1})
+
+// // CREATE INDEXING 
+// db.website1.createIndex({'name':-1})
+
+// db.website1.getIndexes()
+
+// // DROP INDEX
+// db.website.dropIndex({'name':-1})
+
+// db.website1.dropIndex({'name':1})
+
+// // GET INDEXING
+// db.website1.getIndexes()
+
+// db.website1.find().pretty()
+
+// db.website1.createIndex({'name':1})
+
+// db.website1.getIndexes()
+
+// db.website1.dropIndex({'name':1})
--- Semester 5/DBMS/Asgn 9.js
+++ Semester 5/DBMS/Asgn 9.js
@@ -1,44 +1,56 @@
-// 1) INSERT OPERATIONS:
-db.Product.insertOne({pid: 4, pname: "ABC", size:"S", price: 29, rating: 4})
-
-db.Product.insertOne({pid: 1, pname: "DEF", size:"S", price: 110, rating: 4.5})
-
-db.Product.insertMany([
-    {pid: 2, pname: "GHK", size: "M", price: 199, rating: 3.7},
-    {pid: 3, pname: "LKM", size: "L", price: 69, rating: 3.4},
-    {pid: 5, pname: "NHK", size: "L", price: 299, rating: 4.9},
-    {pid: 6, pname: "TUV", size: "S", price: 100, rating: 3.0}
-]);
-
-
-// 2) READ OPERATIONS
-db.Product.find()
-
-db.Product.find({rating: {$gt: 3}})
-
-
-// 3) DELETE OPERATIONS
-db.Product.deleteOne({pname: "GHK"})
-
-// 4) UPDATE OPERATIONS
-db.Product.updateOne({pname: "NHK"}, {$set: {price: 169}})
-
-// 5) OPERATORS
-
-// a-AND
-db.Product.find({
-    price: { $lt: 150},
-    rating: {$gt: 3.5}
-})
-
-// b-OR
-db.Product.find({
-    $or: [
-      {price: {$gt: 100}},
-      {size: "S"}
+//INSERTION
+db.Student.insertOne({
+    rollno: 31253,
+    name: "Ninad",
+    marks: 93,
+    address: [
+        {
+            city: "Pune",
+            pin: 411046,
+        }
     ]
-})
+});
 
-db.Product.findOne({
-    rating: {$lte: 4}
-})
+// SEARCHING
+db.Student.find();
+
+db.Student.find({name:"Ninad"});
+
+db.Student.find({name:"Ninad", marks: {$gt: 90}});
+
+db.Student.find({name:"Ninad"}).limit(1);
+
+db.Student.find({name:"Ninad"}).sort({marks: 1});
+
+db.Student.find({name:"Ninad"}).skip(1);
+
+db.Student.find({name:"Ninad"}).count();
+
+db.Student.find({marks: {$in: [80,90]}}); //$in , $nin
+
+//$gt, $lt, $gte, $lte, $ne
+db.Student.find({
+    //$not,$and
+    $or: [
+        {name: "Ninad"},
+        {marks: {$gt: 80}}
+    ]
+});
+
+db.Student.find(
+    {"address.city": "Pune"}
+);
+
+//DELETION
+db.Student.deleteOne({name: "Ninad"});
+
+//UPDATION
+db.Student.updateOne(
+    {name: "Ninad"},
+    {$set: {"address.city": "mumbai"}}
+);
+
+db.Student.updateOne(
+    {name: "Ninad"},
+    {$set: {marks: 95}}
+);
