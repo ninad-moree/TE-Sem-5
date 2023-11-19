c3c17633ab63b8b0f6942553a8f5ff98633fb7cf
Ninad More <ninadyogeshmore@gmail.com>
2023-11-19 13:01:08 +0530

    tp

--- Semester 5/Python/Macro-Assembler/Pass-1/macro-pass1.py
+++ Semester 5/Python/Macro-Assembler/Pass-1/macro-pass1.py
@@ -1,5 +1,3 @@
-# Author: Shantanu Wable
-
 import re as regex
 import itertools
 import json
@@ -7,68 +5,54 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Funtion to get key from a dictionary based on value
 def get_key(val: str, dict: dict) -> any:
     for key, value in dict.items():
         if val == value:
             return key
 
-# Creating the MDT file 
 with open("Macro-Assembler/Pass-1/output/mdt.asm",'w') as file:
     pass
 file.close()
 
-# Input File(s)
 inputFile = open('Macro-Assembler/Pass-1/input/program.asm', 'r')
-# inputFile = open('Macro-Assembler/Pass-1/input/program2.asm', 'r')
 
 # Output File(s)
 mdtFile = open('Macro-Assembler/Pass-1/output/mdt.asm', 'a')
 
-# Regex pattern to split on occurrence of one or more spaces
 pattern = r'\s+'
 
-# Flags
+
 mSign = False
 mDef = False
 
-# Tables
 mnt = {}
 pntab = {}
 kpdtab = {}
 
-# Storage variables
 macroName = ""
 kpdtabIndex = 1
 kpdtabLoc = 1
 mntIndex = 1
 mdtLoc = 1
 
-# Loop through lines in file
+
 for line in inputFile:
-    # Skip blank lines and remove beginning and trailing whitespace(s)
     if line == '\n': continue
     line = line.strip()
 
-    # Split the line into words
     cmd = regex.split(pattern, line.rstrip())
 
-    # When 'MACRO' is encountered, set the macro signature flag
     if len(cmd) == 1 and cmd[0] == 'MACRO':
         mSign = True
         continue
 
-    # If macro definition has been encountered, process its right next line
     if mSign:
         macroName = cmd.pop(0)
         pntab[macroName] = {}
-        paramCnt = 0
-        
+        paramCnt = 0  
         pp = 0
         kp = 0
-        
-        
-        # Now, cmd contains the list of all the parameters
+
         for parameter in cmd:
             if ',' in parameter:
                 parameter = parameter.replace(',','')
